a23e4f58a3e785e26c8a6b2e731bf888e3f1f20b
Ice5050 <ice50505050@hotmail.com>
2017-12-26 20:03:37 +0700

    Get posts actions

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,9 +1,12 @@
 import {
   ADDING_POST,
-  ADDING_POST_SUUCCESS,
-  ADDING_POST_FAILURE
+  ADDING_POST_SUCCESS,
+  ADDING_POST_FAILURE,
+  GETTING_POSTS,
+  GETTING_POSTS_SUCCESS,
+  GETTING_POSTS_FAILURE
 } from './constants';
-import { PostAPI } from '../utils/api'
+import { PostAPI } from '../utils/api';
 
 export function addPost(post) {
   return dispatch => {
@@ -26,7 +29,7 @@ export function addingPost() {
 
 export function addPostSuccess(post) {
   return {
-    type: ADDING_POST_SUUCCESS,
+    type: ADDING_POST_SUCCESS,
     post
   };
 }
@@ -34,5 +37,37 @@ export function addPostSuccess(post) {
 export function addPostFailure(err) {
   return {
     type: ADDING_POST_FAILURE
+  };
+}
+
+export function getPosts() {
+  return dispatch => {
+    dispatch(gettingPosts);
+    PostAPI.all()
+      .then(data => {
+        dispatch(getPostsSuccess(data));
+      })
+      .catch(err => {
+        dispatch(getPostsFailure(err));
+      });
+  };
+}
+
+export function gettingPosts() {
+  return {
+    type: GETTING_POSTS
+  };
+}
+
+export function getPostsSuccess(posts) {
+  return {
+    type: GETTING_POSTS_SUCCESS,
+    posts
+  };
+}
+
+export function getPostsFailure(err) {
+  return {
+    type: GETTING_POSTS_FAILURE
   };
 }
